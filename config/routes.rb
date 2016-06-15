aae866046b19c340bc0a1263a3e7c93b305cd1f8
noxmwalsh <noxiousthing@gmail.com>
2016-06-14 20:54:16 -0700

    Update routes.rb to include the new tags controller and index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :entity, only: [:create]
+  resources :tags, only: [:index]
   get '/entity/:identifier', to: 'entity#show'
   delete '/entity/:identifier', to: 'entity#destroy'
 
