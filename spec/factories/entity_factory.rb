90bf7d81e6388a9fa456ba668ab3802e83b434f5
noxmwalsh <noxiousthing@gmail.com>
2016-06-14 20:42:46 -0700

    We'll need a entity factory that adds tags for testing the delete action

--- spec/factories/entity_factory.rb
+++ spec/factories/entity_factory.rb
@@ -3,4 +3,11 @@ FactoryGirl.define do
     category { ["Product", "Article", "Widget"].sample }
     identifier { Faker::Number.number(4) }
   end
+
+  factory :entity_with_tags, parent: :entity do
+    before(:create) do |entity, evaluator|
+      random_tags = ["Corgi", "Purple", "Lemur", "Rhino", "Filed"]
+      entity.tag_list.add random_tags.sample(3)
+    end
+  end
 end
