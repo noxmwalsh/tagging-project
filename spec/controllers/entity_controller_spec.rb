b29ff4c4fed6a4ea1293d12fb8f0e523fc522fb4
noxmwalsh <noxiousthing@gmail.com>
2016-06-14 20:15:50 -0700

    Update routes.rb with create entity route and create entity controller spec

---
+++ spec/controllers/entity_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe EntityController, type: :controller do
+
+  describe "POST #create" do
+    it "create an entity and returns a JSON response of the new entity" do
+
+
+    end
+  end
+
+end
